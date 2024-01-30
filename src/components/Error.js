919e014c8ea658b1b7d6dc9eb236f5525455f24e
Tushar Soni <71227235+TusharSoni014@users.noreply.github.com>
2024-01-30 16:41:08 +0530

    added hero section in next js

--- src/components/Error.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Error() {
-  return (
-    <div className='error'>
-        <img src="https://media.tenor.com/IHdlTRsmcS4AAAAC/404.gif" alt="" />
-    </div>
-  )
-}
